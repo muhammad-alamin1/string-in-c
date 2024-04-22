acdfd377dc6c753d9f060778ce6b4aaf8f8fd565
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-04-23 00:24:26 +0600

    string introduction commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# String in C 
+
+* It is represented as an array of characters termimated by a null character.
---
+++ intro_str.c
@@ -0,0 +1,9 @@
+// introduction to string in c program
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    return 0;
+}
