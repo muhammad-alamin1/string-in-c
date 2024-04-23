38aa08410ad7611c16115235f517922f2fe1ae88
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-04-23 23:15:35 +0600

    initialize string

--- intro_str.c
+++ intro_str.c
@@ -3,7 +3,19 @@
 
 int main(void)
 {
+    char let = 'A';
+    char get_str[] = "Hello";
+    char world[] = {'w', 'o', 'r', 'l', 'd', '\0'};
+    int len;
 
+    printf("%c\n", let);
+    printf("%s\n", get_str);
+    printf("%s\n", world);
+
+    len = 0;
+    while(get_str[len] != '\0'){
+        printf("%c", get_str[len++]);
+    }
 
     return 0;
 }
